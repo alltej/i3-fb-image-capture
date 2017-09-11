31896f792bafdd35e1ad3290299702d900ff6b0c
alltej <allan.tejano@gmail.com>
2017-09-11 16:28:43 -0400

    test

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,8 +1,8 @@
 export var config = {
-    apiKey: <apiKey>,
-    authDomain: <yourauthDomain>,
-    databaseURL: <yourdatabaseURL>,
-    projectId: <yourprojectId>,
-    storageBucket: <yourstoragebucket>,
-    messagingSenderId: <yoursenderIdhere>
+  apiKey: "AIzaSyABuhnwiiEaSIMHinWW8YufLpqHkwrm_IQ",
+  authDomain: "ionic3chat-2a058.firebaseapp.com",
+  databaseURL: "https://ionic3chat-2a058.firebaseio.com",
+  projectId: "ionic3chat-2a058",
+  storageBucket: "ionic3chat-2a058.appspot.com",
+  messagingSenderId: "310935171621"
   };
