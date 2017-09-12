017b97a884993272e8eea0dca85e01b3c4582831
alltej <allan.tejano@gmail.com>
2017-09-12 12:13:57 -0400

    added ionic cordova build android/ios

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -14,25 +14,23 @@ export class SignupPage {
     displayName: ''
   }
   constructor(public navCtrl: NavController, public navParams: NavParams, public userservice: UserProvider,
-              public loadingCtrl: LoadingController) {
-    //, public toastCtrl: ToastController
+              public loadingCtrl: LoadingController, public toastCtrl: ToastController) {
   }
 
   signup() {
-    // var toaster = this.toastCtrl.create({
-    //   duration: 3000,
-    //   position: 'bottom'
-    // });
-    // if (this.newuser.email == '' || this.newuser.password == '' || this.newuser.displayName == '') {
-    //   toaster.setMessage('All fields are required dude');
-    //   toaster.present();
-    // }
-    // else if (this.newuser.password.length < 7) {
-    //   toaster.setMessage('Password is not strong. Try giving more than six characters');
-    //   toaster.present();
-    // }
-    // else
-      {
+    let toaster = this.toastCtrl.create({
+      duration: 3000,
+      position: 'bottom'
+    });
+    if (this.newuser.email == '' || this.newuser.password == '' || this.newuser.displayName == '') {
+      toaster.setMessage('All fields are required');
+      toaster.present();
+    }
+    else if (this.newuser.password.length < 7) {
+      toaster.setMessage('Password is not strong. Try giving more than six characters');
+      toaster.present();
+    }
+    else {
       let loader = this.loadingCtrl.create({
         content: 'Please wait'
       });
@@ -46,34 +44,6 @@ export class SignupPage {
       })
     }
   }
-
-  // signup() {
-  //   var toaster = this.toastCtrl.create({
-  //     duration: 3000,
-  //     position: 'bottom'
-  //   });
-  //   if (this.newuser.email == '' || this.newuser.password == '' || this.newuser.displayName == '') {
-  //     toaster.setMessage('All fields are required dude');
-  //     toaster.present();
-  //   }
-  //   else if (this.newuser.password.length < 7) {
-  //     toaster.setMessage('Password is not strong. Try giving more than six characters');
-  //     toaster.present();
-  //   }
-  //   else {
-  //     let loader = this.loadingCtrl.create({
-  //       content: 'Please wait'
-  //     });
-  //     loader.present();
-  //     this.userservice.adduser(this.newuser).then((res: any) => {
-  //       loader.dismiss();
-  //       if (res.success)
-  //         this.navCtrl.push('ProfilepicPage');
-  //       else
-  //         alert('Error' + res);
-  //     })
-  //   }
-  // }
 
   goback() {
     this.navCtrl.setRoot('LoginPage');
